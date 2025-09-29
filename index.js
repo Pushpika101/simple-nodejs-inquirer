cd2572937c836e8808a8268a8fd2d4b34e197b19
Pushpika Anuradha <e21442@eng.pdn.ac.lk>
2025-09-29 23:51:41 +0530

    Enhance student info input with inquirer prompts

--- index.js
+++ index.js
@@ -1,37 +1,60 @@
+import { log } from 'node:console';
 import fileRead from './libs/readfile.js';
 import fileWrite from './libs/writefile.js';
 import inquirer from 'inquirer';
 import { type } from 'node:os';
 
 let a = true;
+const studentInfo = [];
 
 const myFunction = async () => {
-  do  {const data = await inquirer.prompt([
+  do {
+    const data = await inquirer.prompt([
     
     {
-        type: 'password',
+        type: 'input',
         name: 'user name is',
         message: 'select the name',
-        mask: '*',
+        
     },
-
-
+    {
+        type: 'input',
+        name: 'city',
+        message: 'enter the city',
+        
+    },
+    {
+        type: 'list',
+        name: 'class',
+        message: 'select the class',
+        choices: ['A', 'B', 'C', 'D']
+        
+    },
+    {
+        type: 'checkbox',
+        name: 'subject',
+        message: 'select the subjects',
+        choices: ['java', 'python', 'C', 'react']
+        
+    },
 
     {
         type: 'confirm',
         name: 'check',
         message: 'have any others?',
     }
-])
-    .then(data => {
-        console.log(data);
-        if(!data.check)
-        {
-            a = false;
-        }
-    })
+    ]);
     
-}while(a)}
+    const {check, ...infor} = data;
+    studentInfo.push(infor);
+    if(!check) {
+        a = false;
+    }
+    
+} while(a);
+log(studentInfo);
+}
 
 myFunction();
 
+ 
